55af0d3f007b2814e2ca99b4c331ca76623b2c97
Catalin12 <uzy_electro@yahoo.com>
2020-11-19 11:22:33 +0200

    pairoftwo update

--- src/lab1/FooBarQix.java
+++ src/lab1/FooBarQix.java
@@ -1,2 +1,35 @@
-package lab1;public class FooBarQix {
+package lab1;
+
+public class FooBarQix {
+    public static String compute(int n) {
+        String aux = n + " => ";
+        int cn = n;
+        if(n % 3 == 0) aux = aux + "Foo";
+        if(n % 5 == 0) aux = aux + "Bar";
+        if(n % 7 == 0) aux = aux + "Qix";
+        while(n>0) {
+            if(n%10 == 3) aux = aux + "Foo";
+            if(n%10 == 5) aux = aux + "Bar";
+            if(n%10 == 7) aux = aux + "Qix";
+            n/=10;
+        }
+        if(aux.length() == (int) (Math.log10(cn) + 1) + 4) aux = aux + Integer.toString(cn);
+        aux = aux + "\n";
+        return aux;
+    }
+    /*
+    public static String compute2(int n) {
+
+    }
+     */
+    public static void main(String[] args) {
+        for(int i=1; i<100; i++) System.out.println(compute(i));
+        /*
+        compute2(101);
+        compute2(303);
+        compute2(105);
+        compute2(10101);
+         */
+
+    }
 }
--- src/lab1/PairOfTwo.java
+++ src/lab1/PairOfTwo.java
@@ -1,2 +1,18 @@
-package lab1;public class PairOfTwo {
+package lab1;
+
+public class PairOfTwo {
+
+    public static void main(String[] args) {
+        int[] v = {3, 2, -3, -2, 3, 0};
+        int[] aux = new int[v.length];
+        int count=0;
+        for(int i=0; i<v.length-1; i++)
+        for(int j=i+1; j<v.length; j++) if(v[i] + v[j] == 0 && aux[i] == 0 && aux[j] == 0) {
+            System.out.printf("(%d, %d) => %d\n", v[i], v[j], v[i]+v[j]);
+            aux[i]=1; aux[j]=1;
+            count++;
+        }
+        System.out.print(count);
+
+    }
 }
